c34faaad570f7a337a420ea4e1d9bdcdd41ff54a
Alexander Burkhart <alexander.burkhart@gmail.com>
2020-04-17 17:25:04 -0400

    Cleaned up train.py

--- train.py
+++ train.py
@@ -1,14 +1,41 @@
 import pandas as pd
 import numpy as np
 import datetime as dt
+import logging
+
 from matplotlib import pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+
+from optparse import OptionParser
+
 from util import get_data
 from model import lstm_model
 
-TIME_STEPS = 10
-BATCH_SIZE = 16
+logging.basicConfig()
+log = logging.getLogger('train')
+log.setLevel(logging.INFO)
+
+TIME_STEPS = 60
+BATCH_SIZE = 20
+
+parser = OptionParser()
+
+parser.add_option('--sd', '--startdate', dest='sd', default='2007-01-01', help='Start date for training.')
+parser.add_option('--ed', '--enddate', dest='ed', default='2008-01-01', help='End date for training.')
+parser.add_option('--tc', '--traincols', dest='train_cols', 
+                    default=['Open','High','Low','Close','Volume'], help='data cols to train on')
+
+(options, args) = parser.parse_args()
+
+sd = dt.datetime.strptime(options.sd, '%Y-%m-%d')
+ed = dt.datetime.strptime(options.ed, '%Y-%m-%d')
+dates = pd.date_range(sd, ed)
+
+train_cols = options.train_cols
+data_sym = 'AAPL'
+train_size = 0.8
+test_size = 0.2
 
 def plot_stock(df):
     plt.figure()
@@ -38,7 +65,6 @@ def build_timeseries(mat, y_col_index):
     dim_1 = mat.shape[1]
     x = np.zeros((dim_0, TIME_STEPS, dim_1))
     y = np.zeros((dim_0,))
-
     for i in range(dim_0):
         x[i] = mat[i:TIME_STEPS+i]
         y[i] = mat[TIME_STEPS+i, y_col_index]
@@ -52,22 +78,27 @@ def trim_dataset(mat, batch_size):
     else:
         return mat
 
-sd = dt.datetime(2007,1,1)
-ed = dt.datetime(2008,1,1)
+log.info('Creating model...')
 
-dates = pd.date_range(sd, ed)
+model = lstm_model((BATCH_SIZE, TIME_STEPS, len(train_cols)))
 
-train_cols = ['Open','High','Low','Close','Volume']
+log.info('Done')
+print()
 
-df_aapl = get_data('AAPL', dates, data_cols=train_cols, sing=True)
+log.info('Loading and preprocessing...')
+log.info('Loading data...')
 
-df_train, df_test = train_test_split(df_aapl, train_size=0.8, test_size=0.2, shuffle=False)
-print('Train and Test size', len(df_train), len(df_test))
+df_data = get_data(data_sym, dates, data_cols=train_cols, sing=True)
+df_train, df_test = train_test_split(df_data, train_size=train_size, test_size=test_size, shuffle=False)
+
+log.info('Data loaded.')
+print()
+log.info('Preprocessing data...')
 
 min_max_scaler = MinMaxScaler()
 x_train = min_max_scaler.fit_transform(df_train.loc[:,train_cols])
 x_test = min_max_scaler.transform(df_test.loc[:,train_cols])
-
+ 
 x_t, y_t = build_timeseries(x_train, 3)
 x_t = trim_dataset(x_t, BATCH_SIZE)
 y_t = trim_dataset(y_t, BATCH_SIZE)
@@ -75,20 +106,19 @@ x_temp, y_temp = build_timeseries(x_test, 3)
 x_val, x_test_t = np.split(trim_dataset(x_temp, BATCH_SIZE),2)
 y_val, y_test_t = np.split(trim_dataset(y_temp, BATCH_SIZE),2)
 
-model = lstm_model((BATCH_SIZE, TIME_STEPS, x_t.shape[2]))
+log.info('Done preprocessing.')
+print()
+logging.info('Training model...')
 history = model.fit(x_t, y_t, epochs=10, verbose=2, batch_size=BATCH_SIZE,
-                    shuffle=False, validation_data=(trim_dataset(x_val, BATCH_SIZE),
-                    trim_dataset(y_val, BATCH_SIZE)))
-print('Saving model.')
-#model.save('model.h5')
+                     shuffle=False, validation_data=(trim_dataset(x_val, BATCH_SIZE),
+                     trim_dataset(y_val, BATCH_SIZE)))
+log.info('Done training.')
+print()
+log.info('Predicting...')
 y_pred = model.predict(trim_dataset(x_test_t, BATCH_SIZE), batch_size=BATCH_SIZE)
 y_pred = y_pred.flatten()
 y_test_t = trim_dataset(y_test_t, BATCH_SIZE)
-
+ 
 y_pred_org = (y_pred * min_max_scaler.data_range_[3]) + min_max_scaler.data_min_[3]
 y_test_t_org = (y_test_t * min_max_scaler.data_range_[3]) + min_max_scaler.data_min_[3]
 plot_pred(y_pred_org, y_test_t_org)
-
-#plot_stock(df_aapl)
-
-
