85abc5a5fd302bf695cd769a5c6a2b56abe15c2f
Alexander Burkhart <alexander.burkhart@gmail.com>
2020-04-19 12:52:26 -0400

    Added more params and auto model save.

--- train.py
+++ train.py
@@ -19,16 +19,21 @@ logging.basicConfig()
 log = logging.getLogger('train')
 log.setLevel(logging.INFO)
 
-TIME_STEPS = 60
-BATCH_SIZE = 20
-
 parser = OptionParser()
-
+parser.add_option('-s', '--sym', dest='sym', default='BAC', help='Stock symbol to predict.')
 parser.add_option('--sd', '--startdate', dest='sd', default='2007-01-01', help='Start date for training.')
 parser.add_option('--ed', '--enddate', dest='ed', default='2012-01-01', help='End date for training.')
 parser.add_option('--tc', '--traincols', dest='train_cols', 
                     default=['Open','High','Low','Close','Volume'], help='Data cols to train on.')
-parser.add_option('--mp', '--modelpath', dest='model_path', default='models/model.h5', help='Path to model.')
+parser.add_option('--md', '--modeldir', dest='model_dir', default='models/', help='Directory to model.')
+parser.add_option('-e', '--epochs', dest='epochs', default=300, help='Number of epochs to train.')
+parser.add_option('-b', '--batchsize', dest='batch_size', default=16, help='Batch size to train.')
+parser.add_option('-t', '--timesteps', dest='time_steps', default=60, help='Time steps to train.')
+parser.add_option('--trs', '--trainsize', dest='train_size', default=0.8, help='Percentage of training data.')
+parser.add_option('--tes', '--testsize', dest='test_size', default=0.2, help='Percentage of testing data.')
+parser.add_option('--mn', '--model_name', dest='model_name', default='model.h5', help='Name of model.')
+
+
 
 (options, args) = parser.parse_args()
 
@@ -36,11 +41,15 @@ sd = dt.datetime.strptime(options.sd, '%Y-%m-%d')
 ed = dt.datetime.strptime(options.ed, '%Y-%m-%d')
 dates = pd.date_range(sd, ed)
 
-model_path = options.model_path
+model_dir = options.model_dir
 train_cols = options.train_cols
-data_sym = 'BAC'
-train_size = 0.8
-test_size = 0.2
+data_sym = options.sym
+model_name = options.model_name
+train_size = float(options.train_size)
+test_size = float(options.test_size)
+epochs = int(options.epochs)
+batch_size = int(options.batch_size)
+time_steps = int(options.time_steps)
 
 def plot_stock(df):
     plt.figure()
@@ -66,18 +75,18 @@ def plot_pred(y_pred, y_org):
     plt.show()
 
 def build_timeseries(mat, y_col_index):
-    dim_0 = mat.shape[0] - TIME_STEPS
+    dim_0 = mat.shape[0] - time_steps
     dim_1 = mat.shape[1]
     print(mat.shape[0])
-    x = np.zeros((dim_0, TIME_STEPS, dim_1))
+    x = np.zeros((dim_0, time_steps, dim_1))
     y = np.zeros((dim_0,))
     for i in range(dim_0):
-        x[i] = mat[i:TIME_STEPS+i]
-        y[i] = mat[TIME_STEPS+i, y_col_index]
+        x[i] = mat[i:time_steps+i]
+        y[i] = mat[time_steps+i, y_col_index]
     print('length of time-series i/o',x.shape,y.shape)
     return x,y
 
-def trim_dataset(mat, batch_size):
+def trim_dataset(mat):
     no_of_rows_drop = mat.shape[0]%batch_size
     if(no_of_rows_drop > 0):
         return mat[:-no_of_rows_drop]
@@ -86,7 +95,7 @@ def trim_dataset(mat, batch_size):
 
 def preprocess(x, y_col_index):
     x,y = build_timeseries(x, y_col_index)
-    return trim_dataset(x, BATCH_SIZE), trim_dataset(y, BATCH_SIZE)
+    return trim_dataset(x), trim_dataset(y)
 
 log.info('Loading and preprocessing...')
 log.info('Loading data...')
@@ -112,30 +121,42 @@ y_val, y_test_t = np.split(y_temp, 2)
 log.info('Done preprocessing.')
 print()
 
+model_path = model_dir+model_name
 if os.path.isfile(model_path):
     log.info('Loading model...')
     
     model = load_model(model_path)
+
+    log.info('Loaded model.')
 else:
     log.info('Creating model...')
 
-    model = lstm_model((BATCH_SIZE, TIME_STEPS, len(train_cols)))
+    model = lstm_model((batch_size, time_steps, len(train_cols)))
 
     log.info('Done.')
     print()
     log.info('Training model...')
     
-    model.fit(x_t, y_t, epochs=10, verbose=2, batch_size=BATCH_SIZE,
-                shuffle=False, validation_data=(trim_dataset(x_val, BATCH_SIZE),
-                trim_dataset(y_val, BATCH_SIZE)))
+    model.fit(x_t, y_t, epochs=epochs, verbose=2, batch_size=batch_size,
+                shuffle=False, validation_data=(trim_dataset(x_val),
+                trim_dataset(y_val)))
 
-log.info('Done.')
+    log.info('Done training...')
+    print()
+    log.info('Saving model...')
+   
+    if not os.path.exists(model_dir):
+        os.mkdir(model_dir)
+    model.save(model_path)
+
+    log.info('Saved model.')
+
 print()
 log.info('Predicting...')
 
-y_pred = model.predict(trim_dataset(x_test_t, BATCH_SIZE), batch_size=BATCH_SIZE)
+y_pred = model.predict(trim_dataset(x_test_t), batch_size=batch_size)
 y_pred = y_pred.flatten()
-y_test_t = trim_dataset(y_test_t, BATCH_SIZE)
+y_test_t = trim_dataset(y_test_t)
 
 y_pred_org = (y_pred * min_max_scaler.data_range_[3]) + min_max_scaler.data_min_[3]
 y_test_t_org = (y_test_t * min_max_scaler.data_range_[3]) + min_max_scaler.data_min_[3]
